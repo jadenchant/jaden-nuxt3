4ec4c2d49f924ecd80d852ff6060a70f69eb5e74
Jaden Chant <chant1@stolaf.edu>
2025-01-20 19:05:27 -0600

    update packages, start revworsear

---
+++ pages/projects/reverse-word-search.vue
@@ -0,0 +1,28 @@
+<template></template>
+
+<script setup lang="ts">
+definePageMeta({
+  title: 'Reverse Word Search',
+});
+
+useSeoMeta({
+  title: 'Reverse Word Search',
+  ogTitle: 'Reverse Word Search Project',
+  author: 'Jaden Chant',
+  description: "Jaden Chant's Reverse Word Search",
+  ogDescription: "Jaden Chant's Reverse Word Search",
+  ogImage: 'https://jadenchant.com/_ipx/w_1500/images/jaden_nyc.png',
+});
+
+// b - a - -     angle   blank
+// s - - - -     leap    link
+// t - - - -     sink    snack
+// - * - - -     stop    tag
+// - - - l s
+
+const matrix = useCookie('matrix');
+
+const defaultMatrix = 'b-a--;s----;t----;-*---;---ls;';
+
+matrix.value = matrix.value || defaultMatrix;
+</script>
