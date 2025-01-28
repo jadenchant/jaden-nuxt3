efcee1440a4fdbd410a783b5a31daaf8d70fa75c
Jaden Chant <chant1@stolaf.edu>
2025-01-27 18:23:45 -0600

    matrix vis complete

--- pages/projects/reverse-word-search.vue
+++ pages/projects/reverse-word-search.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="grid">
-    <p>a</p>
+  <div class="container mx-auto p-4">
+    <div class="grid grid-cols-5">
+      <div
+        v-for="(cell, index) in matrixArray.flat()"
+        :key="index"
+        class="p-2 text-center text-2xl border border-gray-300"
+      >
+        {{ cell }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +26,14 @@ useSeoMeta({
   ogImage: "https://jadenchant.com/_ipx/w_1500/images/jaden_nyc.png",
 });
 
+const stringToMatrix = (str: string) => {
+  return str.split(";").map((row) => row.split(""));
+};
+
+const matrixToString = (matrix: Array<Array<string>>) => {
+  return matrix.map((row) => row.join("")).join(";");
+};
+
 // b - a - -     angle   blank
 // s - - - -     leap    link
 // t - - - -     sink    snack
@@ -29,4 +45,6 @@ const matrix = useCookie("matrix");
 const defaultMatrix = "b-a--;s----;t----;-*---;---ls;";
 
 matrix.value = matrix.value || defaultMatrix;
+
+const matrixArray = ref(stringToMatrix(matrix.value));
 </script>
