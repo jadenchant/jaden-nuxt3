1d1c5ee55812a35ae473395514a82a4554f79ee5
Jaden Chant <chant1@stolaf.edu>
2025-07-27 20:48:04 -0500

    starting on the UI

--- app/components/Spotify.vue
+++ app/components/Spotify.vue
@@ -1,3 +1,21 @@
-<template></template>
+<template>
+  <div v-if="!error && data">
+    <p>{{ data.item }}</p>
+    <NuxtImg
+      v-if="data.item.album.images[1].url"
+      :src="data.item.album.images[1].url"
+      alt="Album"
+      class="max-h-52"
+      sizes="sm:250px lg:300px"
+      format="webp"
+      preload
+    />
+  </div>
+  <p v-if="error">{{ error }}</p>
+</template>
 
-<script setup></script>
+<script setup lang="ts">
+const { data, error } = await useFetch<SpotifyCurrentlyPlaying>(
+  "/api/spotify/playing",
+);
+</script>
--- app/utils/types.ts
+++ app/utils/types.ts
@@ -3,3 +3,79 @@ export type resToken = {
   access_token: string;
   refresh_token: string;
 };
+
+export interface SpotifyCurrentlyPlaying {
+  is_playing: boolean;
+  timestamp: number;
+  context: {
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    type: string;
+    uri: string;
+  };
+  progress_ms: number;
+  item: {
+    album: {
+      album_type: string;
+      artists: SpotifyArtist[];
+      available_markets: string[];
+      external_urls: {
+        spotify: string;
+      };
+      href: string;
+      id: string;
+      images: SpotifyImage[];
+      name: string;
+      release_date: string;
+      release_date_precision: string;
+      total_tracks: number;
+      type: string;
+      uri: string;
+    };
+    artists: SpotifyArtist[];
+    available_markets: string[];
+    disc_number: number;
+    duration_ms: number;
+    explicit: boolean;
+    external_ids: {
+      isrc: string;
+    };
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    is_local: boolean;
+    name: string;
+    popularity: number;
+    preview_url: string | null;
+    track_number: number;
+    type: string;
+    uri: string;
+  };
+  currently_playing_type: string;
+  actions: {
+    disallows: {
+      [key: string]: boolean;
+    };
+  };
+}
+
+export interface SpotifyArtist {
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+}
+
+export interface SpotifyImage {
+  height: number;
+  url: string;
+  width: number;
+}
