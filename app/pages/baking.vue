c22210a7c431baee6227f612d39ebb73f93617b7
Jaden Chant <chant1@stolaf.edu>
2026-06-20 14:26:14 -0500

    added baked goods

--- app/pages/baking.vue
+++ app/pages/baking.vue
@@ -22,6 +22,57 @@
     </NuxtLink>
 
     <NuxtLink
+      to="https://preppykitchen.com/molten-chocolate-cake/"
+      target="_blank"
+      class="flex flex-col w-min"
+    >
+      <NuxtImg
+        src="images/lava-cake.jpg"
+        alt="Chocolate Molten Lava Cake"
+        class="h-[500px] md:h-[300px] lg:h-[400px] xl:h-[500px] w-auto object-cover z-10 max-w-none"
+        sizes="sm:250px md:300px lg:500px"
+        format="webp"
+      />
+      <p class="text-xl mt-2 px-2 w-full">Chocolate Molten Lava Cakes</p>
+    </NuxtLink>
+
+    <NuxtLink
+      to="https://preppykitchen.com/strawberry-rhubarb-pie/"
+      target="_blank"
+      class="flex flex-col w-min"
+    >
+      <NuxtImg
+        src="images/strawberry-rhubarb.jpg"
+        alt="Strawberry Rhubarb Pie"
+        class="h-[500px] md:h-[300px] lg:h-[400px] xl:h-[500px] w-auto object-cover z-10 max-w-none"
+        sizes="sm:250px md:300px lg:500px"
+        format="webp"
+        preload
+      />
+      <p class="text-xl mt-2 px-2 w-full leading-snug">
+        Strawberry Rhubarb Pie
+      </p>
+    </NuxtLink>
+  </div>
+  <div
+    class="flex flex-col md:flex-row items-center md:items-start justify-between lg:mx-20 z-10 gap-10"
+  >
+    <NuxtLink
+      to="https://preppykitchen.com/creme-brulee/"
+      target="_blank"
+      class="flex flex-col w-min"
+    >
+      <NuxtImg
+        src="images/creme-brulee.jpg"
+        alt="Crème Brûlée"
+        class="h-[500px] md:h-[300px] lg:h-[400px] xl:h-[500px] w-auto object-cover z-10 max-w-none"
+        sizes="sm:250px md:300px lg:500px"
+        format="webp"
+      />
+      <p class="text-xl mt-2 px-2 w-full">Crème Brûlée</p>
+    </NuxtLink>
+
+    <NuxtLink
       to="https://preppykitchen.com/cheesecake-recipe/"
       target="_blank"
       class="flex flex-col w-min"
@@ -38,18 +89,19 @@
     </NuxtLink>
 
     <NuxtLink
-      to="https://preppykitchen.com/creme-brulee/"
+      to="https://preppykitchen.com/cheesecake-recipe/"
       target="_blank"
       class="flex flex-col w-min"
     >
       <NuxtImg
-        src="images/creme-brulee.jpg"
-        alt="Crème Brûlée"
+        src="images/cheesecake.jpg"
+        alt="Biscoff Cheesecake"
         class="h-[500px] md:h-[300px] lg:h-[400px] xl:h-[500px] w-auto object-cover z-10 max-w-none"
         sizes="sm:250px md:300px lg:500px"
         format="webp"
+        preload
       />
-      <p class="text-xl mt-2 px-2 w-full">Crème Brûlée</p>
+      <p class="text-xl mt-2 px-2 w-full">Classic Cheesecake</p>
     </NuxtLink>
   </div>
 </template>
